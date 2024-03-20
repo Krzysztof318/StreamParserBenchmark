afcae39dc1bd43376ce21f4285675185e49b5382
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-03-20 21:15:05 +0100

    init

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,110 @@
+﻿
+using System.Buffers;
+using System.IO.Pipelines;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using ConsoleApp5;
+
+// DefaultConfig.Instance
+//     .AddJob(Job
+//         .MediumRun
+//         .WithLaunchCount(1)
+//         .WithToolchain(InProcessEmitToolchain.Instance));
+
+BenchmarkRunner.Run<Bench>();
+
+[SimpleJob(RuntimeMoniker.Net47)]
+[SimpleJob(RuntimeMoniker.Net80)]
+[MemoryDiagnoser]
+public class Bench
+{
+    string data = File.ReadAllText(@"test.txt");
+    
+    public Bench()
+    {
+        
+    }
+
+    [Benchmark]
+    public async Task StephenProposition()
+    {
+        var stream = LoadDataToStream();
+        var class1 = new StephenProposition();
+        var reader = PipeReader.Create(stream);
+        await foreach (var jsonDocument in class1.ParseJson(reader))
+        {
+            // we need to parse to business object
+            var jsonObj = jsonDocument.Deserialize<MyObjectJson>();
+            jsonDocument.Dispose();
+        }
+    }
+
+    [Benchmark]
+    public async Task StreamJsonParser()
+    {
+        var stream = LoadDataToStream();
+        var parser = new StreamJsonParser();
+        var result = parser.ParseAsync(stream);
+        await foreach (var item in result)
+        {
+            // here we can parse just to business object
+            var jsonObj = JsonSerializer.Deserialize<MyObjectJson>(item);
+        }
+    }
+
+    private MemoryStream LoadDataToStream()
+    {
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
+        return stream;
+    }
+}
+
+public class MyObjectJson
+{
+    public object[] candidates { get; set; }
+}
+
+public class StephenProposition
+{
+    public async IAsyncEnumerable<JsonDocument> ParseJson(PipeReader reader)
+    {
+        while (true)
+        {
+            ReadResult result = await reader.ReadAsync();
+            ReadOnlySequence<byte> buffer = result.Buffer;
+
+            while (!buffer.IsEmpty && TryParseJson(ref buffer, out JsonDocument jsonDocument))
+            {
+                yield return jsonDocument;
+            }
+
+            if (result.IsCompleted)
+            {
+                break;
+            }
+
+            reader.AdvanceTo(buffer.Start, buffer.End);
+        }
+
+        reader.Complete();
+    }
+
+    bool TryParseJson(ref ReadOnlySequence<byte> buffer, out JsonDocument jsonDocument)
+    {
+        var reader = new Utf8JsonReader(buffer, isFinalBlock: false, default);
+
+        if (JsonDocument.TryParseValue(ref reader, out jsonDocument))
+        {
+            buffer = buffer.Slice(reader.BytesConsumed);
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ ConsoleApp5/readme.md
@@ -0,0 +1,10 @@
+﻿| Method             | Job                | Runtime            | Mean     | Error     | StdDev    | Gen0     | Gen1    | Gen2    | Allocated  |
+|------------------- |------------------- |------------------- |---------:|----------:|----------:|---------:|--------:|--------:|-----------:|
+| StephenProposition | .NET 8.0           | .NET 8.0           | 1.640 ms | 0.0324 ms | 0.0304 ms | 156.2500 | 50.7813 | 50.7813 |  725.52 KB |
+| StreamJsonParser   | .NET 8.0           | .NET 8.0           | 1.176 ms | 0.0076 ms | 0.0068 ms | 287.1094 | 50.7813 | 50.7813 | 1417.93 KB |
+| StephenProposition | .NET Framework 4.7 | .NET Framework 4.7 | 6.415 ms | 0.0363 ms | 0.0284 ms | 148.4375 | 78.1250 | 46.8750 |  730.42 KB |
+| StreamJsonParser   | .NET Framework 4.7 | .NET Framework 4.7 | 4.432 ms | 0.0139 ms | 0.0123 ms | 335.9375 | 85.9375 | 46.8750 | 1685.39 KB |
+
+for 164 KB file
+
+StephenProposition doesn't work with sse data when input contains also other data than json. It's not a problem for StreamJsonParser.
